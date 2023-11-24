acd818c5c3d2f965049e0557bdee93b1db84a96d
Reza Khalilian <reza.kh.adel@gmail.com>
2023-11-24 18:59:10 +0330

    update setting.py :change database of sqlite3 to postgress

--- src/shop/settings.py
+++ src/shop/settings.py
@@ -85,11 +85,20 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('POSTGRES_DB'),
+       'USER': os.getenv('POSTGRES_USER'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+       'HOST': os.getenv('POSTGRES_HOST'),
+       'PORT': os.getenv('POSTGRES_PORT'),
+   }
 }
+
 
 
 # Password validation
