14d4393b9998764fbabb51c434ab6c8908c3b2d3
Reza Khalilian <reza.kh.adel@gmail.com>
2023-12-22 15:28:14 +0330

    update expire sms code  link style

--- src/accountt/views.py
+++ src/accountt/views.py
@@ -527,7 +527,7 @@ class Check_Phone_forget_password(APIView):
                             </div>
                              <span class="text-danger" id="error" style="display:none;"></span><br>
                         
-                            <p id="link">  ارسال مجدد کد در <span id='time'> (04:00) </span></p>  
+                            <p id="link text-info" >  ارسال مجدد کد در <span id='time'> (04:00) </span></p>  
                     </div>
             """ 
             return Response({'phone':phone,'password':html},status=status.HTTP_200_OK)
